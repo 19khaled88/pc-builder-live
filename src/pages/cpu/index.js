cf925e2570b2837362a7f986f35114db012a9469
19khaled88 <69922782+19khaled88@users.noreply.github.com>
2023-08-01 01:04:49 +0600

    attach live link again

--- src/pages/cpu/index.js
+++ src/pages/cpu/index.js
@@ -32,7 +32,7 @@ export async function getStaticProps() {
       }
     }
   }
-  const res = await fetch(`${process.env.LIVE_URL}`)
+  const res = await fetch(`https://pc-builder-live-seven.vercel.app`)
     const data = await res.json()
    
     return {
