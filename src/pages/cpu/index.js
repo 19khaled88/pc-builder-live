6bc14034fc45a0dc89530e8a5a08f4f2aea422ab
19khaled88 <69922782+19khaled88@users.noreply.github.com>
2023-08-01 08:34:20 +0600

    build by local link

--- src/pages/cpu/index.js
+++ src/pages/cpu/index.js
@@ -22,14 +22,14 @@ CpuPage.getLayout = function getLayout(page) {
 }
 
 export async function getStaticProps() {
-  //   if(typeof window === undefined){
-  //     return{
-  //       props:{
-  //         display:[]
-  //       }
-  //     }
-  //   }
-  const res = await fetch(`https://jsonplaceholder.typicode.com/users`)
+    if(typeof window === undefined){
+      return{
+        props:{
+          display:[]
+        }
+      }
+    }
+  const res = await fetch(`${process.env.LOCAL_URL}`)
   const data = await res.json()
 
   return {
